62ea8e550ec85f3d11354bea2f2054dabb263f99
Vladislav <vlad.satonin74@gmail.com>
2022-06-07 00:06:05 +0500

    test(tests): add test to calc all zero & all frames 1 pin

--- src/tests/bowling.test.ts
+++ src/tests/bowling.test.ts
@@ -17,6 +17,18 @@ describe('test Class Bowling for working', () => {
     expect(console.log).toHaveBeenCalledWith(testMsg);
   });
 
+  // test showing result 0, if you do not knock out more than one pin
+  it('should return 0 for a zeros in all frame', () => {
+    rollMore(20, 0);
+    expect(game.score).toEqual(0);
+  })
+
+  // tests showing result 20, if in each round (in two throws) one pin is knocked out
+  it('should return 20 for a ones in all frame', () => {
+    rollMore(20, 1);
+    expect(game.score).toEqual(20);
+  })
+
   it('should return 10 when hitting a strike', () => {
     game.roll(10);
     expect(game.score).toEqual<number>(10);
@@ -28,4 +40,8 @@ describe('test Class Bowling for working', () => {
     game.roll(1);
     expect(game.score).toEqual<number>(14);
   })
+
+  const rollMore = (rolls : number, pins : number) : void => {
+    for(let i = 0; i< rolls; i++) game.roll(pins);
+  }
 });
