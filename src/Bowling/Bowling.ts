2fe664a6db65264419d5588921a82fef8c1a35ce
Vladislav <vlad.satonin74@gmail.com>
2022-06-06 23:30:46 +0500

    feat(bowling-class): add get methods score, isStrike, strikeBonus

--- src/Bowling/Bowling.ts
+++ src/Bowling/Bowling.ts
@@ -1,6 +1,37 @@
 import { IBowling } from "../types/types";
+import { checkNumber } from "../services/services";
 
 class Bowling implements IBowling {
+  // values
+  frames: Array<number> = [];
+
+  // methods
+  roll = (pins : number) => this.frames.push(pins);
+
+  get score() {
+    let score : number = 0;
+
+    for(let frameI = 0; frameI < 10; frameI++) {
+      if(this.isStrike(this.frames[frameI])) {
+        score += this.strikeBonus(frameI);
+        continue;
+      }
+
+      // TODO: fill the array with zeros so as not to check for undefined
+      score += checkNumber(this.frames[frameI]);
+    }
+    return score;
+  };
+
+  // bonus methods
+  strikeBonus = (rollIndex : number) => {
+    return checkNumber(this.frames[rollIndex + 1]) + checkNumber(this.frames[rollIndex + 2]) + 10;
+  };
+
+  // boolean methods
+  isStrike = (frameScore : number) => frameScore === 10;
+
+  //log methods
   logToConsole = (msg : string) => console.log(msg || 'logToConsole');
 }
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,7 @@
 export interface IBowling {
+  frames: Array<number>;
+  roll: (pins: number) => void;
+  strikeBonus: (rollIndex : number) => number;
+  isStrike: (frameScore : number) => boolean;
   logToConsole: (msg : string) => void;
 }
